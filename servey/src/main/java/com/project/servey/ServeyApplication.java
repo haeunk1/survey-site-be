acf0d85c4bf915ead69c9afe39a8a81e07efa41b
Haeun Kim <53562331+haeunk1@users.noreply.github.com>
2024-10-03 13:19:45 +0900

    init commit

---
+++ servey/src/main/java/com/project/servey/ServeyApplication.java
@@ -0,0 +1,13 @@
+package com.project.servey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeyApplication.class, args);
+	}
+
+}
