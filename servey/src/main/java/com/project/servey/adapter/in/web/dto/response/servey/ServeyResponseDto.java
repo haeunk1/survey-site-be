0264311099a36708de18099196363f04de1b5e74
Haeun Kim <haeun8398@gmail.com>
2024-11-07 15:42:53 +0900

    find,create test작성

--- servey/src/main/java/com/project/servey/adapter/in/web/dto/response/servey/ServeyResponseDto.java
+++ servey/src/main/java/com/project/servey/adapter/in/web/dto/response/servey/ServeyResponseDto.java
@@ -2,6 +2,8 @@ package com.project.servey.adapter.in.web.dto.response.servey;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.project.servey.adapter.out.persistence.entity.constant.ServeyType;
 
 import lombok.*;
@@ -9,6 +11,7 @@ import lombok.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ServeyResponseDto {
     private Long serveyId;
     private Long memberId;
@@ -16,11 +19,29 @@ public class ServeyResponseDto {
     private ServeyType type;
     private int perPoint;
     private int limitSubmit;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startdate;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime enddate;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private String deleteYn;
+
+    public static ServeyResponseDto of(Long memberId, String title, ServeyType type
+    , int perPoint, int limitSubmit, LocalDateTime startdate,LocalDateTime enddate){
+        return builder()
+        .memberId(memberId)
+        .title(title)
+        .perPoint(perPoint)
+        .type(type)
+        .limitSubmit(limitSubmit)
+        .startdate(startdate)
+        .enddate(enddate)
+        .createdAt(LocalDateTime.now())
+        .deleteYn("N")
+        .build();
+    }
 
     public static ServeyResponseDto of(Long serveyId, Long memberId, String title, ServeyType type
     , int perPoint, int limitSubmit, LocalDateTime startdate,LocalDateTime enddate){
--- servey/src/test/java/com/project/servey/adapter/in/web/controller/ServeyControllerTest.java
+++ servey/src/test/java/com/project/servey/adapter/in/web/controller/ServeyControllerTest.java
@@ -9,11 +9,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.servey.adapter.in.web.dto.response.servey.ServeyResponseDto;
 import com.project.servey.adapter.out.persistence.entity.constant.ServeyType;
+import com.project.servey.application.command.servey.CreateServeyCommand;
 import com.project.servey.application.command.servey.FindServeyCommand;
 import com.project.servey.application.port.in.servey.CreateServeyUseCase;
 import com.project.servey.application.port.in.servey.DeleteServeyUseCase;
@@ -22,6 +25,7 @@ import com.project.servey.application.port.in.servey.UpdateServeyUseCase;
 import com.project.servey.config.security.SecurityConfig;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.when;
 import static org.mockito.Mockito.doReturn;
@@ -37,37 +41,53 @@ public class ServeyControllerTest {
 
     //컨트롤러의 api테스트를 위해 사용(서블릿 컨테이너의 구동없이 가상의 mvc환경에서 모의 httml서블릿을 요청하는 유틸리티 클래스)
     @Autowired MockMvc mockMvc;
-    @MockBean CreateServeyUseCase createServeyUseCase;
     @MockBean FindServeyUseCase findServeyUseCase; //mock객체를 주입, 
+    @MockBean CreateServeyUseCase createServeyUseCase;
     @MockBean DeleteServeyUseCase deleteServeyUseCase;
     @MockBean UpdateServeyUseCase updateServeyUseCase;
 
     @Test
     void testFindServey() throws Exception{
-        long serveyId = 1L;
-        FindServeyCommand findServeyCommand = FindServeyCommand.of(serveyId);
-        ServeyResponseDto serveyResponseDto = ServeyResponseDto.of(serveyId, 1L, "title1", ServeyType.OX, 10, 10, LocalDateTime.now(), LocalDateTime.now());
+        ServeyResponseDto serveyResponseDto = ServeyResponseDto.of(1L, 1L, "title1", ServeyType.OX, 10, 10, LocalDateTime.now(), LocalDateTime.now());
 
         //given
         //테스트 과정에서 맡을 동작을 정의
-        given(findServeyUseCase.findServey(eq(findServeyCommand))).willReturn(serveyResponseDto);
+        given(findServeyUseCase.findServey(any(FindServeyCommand.class)))
+        .willReturn(serveyResponseDto);
 
         //when&then
         MvcResult result = mockMvc.perform(
             get("/servey/1"))
             .andExpect(status().isOk())
-            //.andExpect(jsonPath("$.title").exists())
-            //.andExpect(jsonPath("$.memberId").exists())
+            .andExpect(jsonPath("$.title").exists())
+            .andExpect(jsonPath("$.memberId").exists())
             .andDo(print())
             .andReturn();
 
             System.out.println(result.getResponse().getContentAsString());
-       
     }
 
     @Test
     void testCreateServey() throws Exception{
+        //Mock 객체에서 특정 메서드가 실행되는 경우 실제 Return을 줄 수 없기 때문에 가정 사항을 만들어줌
+        LocalDateTime customDateTime = LocalDateTime.of(2024, 11, 7, 14, 30, 45, 0);
+
+        ServeyResponseDto serveyResponseDto = ServeyResponseDto.of(1L, "title1", ServeyType.OX, 10, 10, customDateTime,customDateTime);
+        given(createServeyUseCase.createServey(any(CreateServeyCommand.class)))
+        .willReturn(serveyResponseDto);
+
+        //json 요청 바디 설정
+        String requestBody = new ObjectMapper().writeValueAsString(serveyResponseDto);
         
+        mockMvc.perform(
+            post("/servey/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+        
+
     }
 
     @Test
